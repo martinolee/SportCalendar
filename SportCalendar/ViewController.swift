527d5f373cb6a29c0b5a130a9ac9cc039f39b955
Soo-Han Lee <martino_lee@naver.com>
2018-08-16 23:09:48 +0900

    Data parsing test

--- SportCalendar/ViewController.swift
+++ SportCalendar/ViewController.swift
@@ -11,17 +11,31 @@ import Alamofire
 import SwiftyJSON
 
 class ViewController: UIViewController {
-
+    
+    var apiSoccerseasons: NSArray = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let url = "https://api.football-data.org/v1/soccerseasons"
+        let apiURI: URL! = URL(string: url)
+        let apidata = try! Data(contentsOf: apiURI)
+        let log = NSString(data: apidata, encoding: String.Encoding.utf8.rawValue) ?? ""
+        NSLog("API Result =\( log )")
+        
+        do {
+            apiSoccerseasons = try JSONSerialization.jsonObject(with: apidata, options: []) as! NSArray
+        } catch {
+            
+        }
+        
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
-
